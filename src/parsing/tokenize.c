7a10cf44ba4f885b93299b43444d781e658f9e10
kievaughn <103777114+kievaughn@users.noreply.github.com>
2025-08-20 14:22:50 +0300

    Handle quoted expansion and heredoc cleanup

--- src/parsing/tokenize.c
+++ src/parsing/tokenize.c
@@ -13,118 +13,117 @@
 #include "../libft/libft.h"
 #include "minishell.h"
 
-static size_t	skip_token(char const *s, size_t i, char c)
+static size_t   skip_token(char const *s, size_t i, char c)
 {
-	char	quote;
+        char    quote;
 
-	quote = 0;
-	while (s[i])
-	{
-		if (!quote && (s[i] == '"' || s[i] == '\''))
-		{
-			quote = s[i++];
-			while (s[i] && s[i] != quote)
-				i++;
-			if (s[i] == quote)
-				i++;
-			quote = 0;
-			continue ;
-		}
-		if (!quote && s[i] == c)
-			break ;
-		i++;
-	}
-	return (i);
+        quote = 0;
+        while (s[i])
+        {
+                if (!quote && (s[i] == '"' || s[i] == '\''))
+                {
+                        quote = s[i++];
+                        while (s[i] && s[i] != quote)
+                                i++;
+                        if (s[i] == quote)
+                                i++;
+                        quote = 0;
+                        continue ;
+                }
+                if (!quote && s[i] == c)
+                        break ;
+                i++;
+        }
+        return (i);
 }
 
-static size_t	token_count(char const *s, char c)
+static size_t   token_count(char const *s, char c)
 {
-	size_t	i;
-	size_t	count;
+        size_t  i;
+        size_t  count;
 
-	i = 0;
-	count = 0;
-	while (s[i])
-	{
-		while (s[i] == c)
-			i++;
-		if (!s[i])
-			break ;
-		count++;
-		i = skip_token(s, i, c);
-	}
-	return (count);
+        i = 0;
+        count = 0;
+        while (s[i])
+        {
+                while (s[i] == c)
+                        i++;
+                if (!s[i])
+                        break ;
+                count++;
+                i = skip_token(s, i, c);
+        }
+        return (count);
 }
 
-static size_t	next_c(char *s, char c)
+static size_t   next_c(char *s, char c)
 {
-	size_t	len;
-	char	quote;
+        size_t  len;
+        char    quote;
 
-	len = 0;
-	quote = 0;
-	while (s[len])
-	{
-		if (!quote && (s[len] == '"' || s[len] == '\''))
-		{
-			quote = s[len++];
-			while (s[len] && s[len] != quote)
-				len++;
-			if (s[len] == quote)
-				len++;
-			quote = 0;
-			continue ;
-		}
-		if (!quote && s[len] == c)
-			break ;
-		len++;
-	}
-	return (len);
+        len = 0;
+        quote = 0;
+        while (s[len])
+        {
+                if (!quote && (s[len] == '"' || s[len] == '\''))
+                {
+                        quote = s[len++];
+                        while (s[len] && s[len] != quote)
+                                len++;
+                        if (s[len] == quote)
+                                len++;
+                        quote = 0;
+                        continue ;
+                }
+                if (!quote && s[len] == c)
+                        break ;
+                len++;
+        }
+        return (len);
 }
 
-static int	fill_arr_from_string(char const *s, char c, char **arr, char **envp)
+static int      fill_arr_from_string(char const *s, char c, char **arr, char **envp)
 {
-	int		i;
-	size_t	len;
-	char	*expanded;
+        int             i;
+        size_t  len;
+        char    *expanded;
 
-	i = 0;
-	while (*s)
-	{
-		while (*s == c)
-			s++;
-		if (!*s)
-			break ;
-		len = next_c((char *)s, c);
-		arr[i] = ft_substr((char *)s, 0, len);
-		if (!arr[i])
-			return (-1);
-		if (arr[i][0] != '\'')
-		{
-			expanded = build_expanded_str(arr[i], envp);
-			free(arr[i]);
-			arr[i] = expanded;
-		}
-		i++;
-		s += len;
-	}
-	return (i);
+        i = 0;
+        while (*s)
+        {
+                while (*s == c)
+                        s++;
+                if (!*s)
+                        break ;
+                len = next_c((char *)s, c);
+                arr[i] = ft_substr((char *)s, 0, len);
+                if (!arr[i])
+                        return (-1);
+                expanded = build_expanded_str(arr[i], envp);
+                free(arr[i]);
+                if (!expanded)
+                        return (-1);
+                arr[i] = expanded;
+                i++;
+                s += len;
+        }
+        return (i);
 }
 
-char	**tokenize_command(char const *s, char c, char **envp)
+char    **tokenize_command(char const *s, char c, char **envp, int **quoted)
 {
-	char	**arr;
-	int		i;
-	int		token_num;
+        char    **arr;
+        int             i;
+        int             token_num;
 
-	if (!s)
-		return (NULL);
-	arr = (char **)malloc((token_count(s, c) + 1) * sizeof(char *));
-	if (!arr)
-		return (NULL);
-	token_num = fill_arr_from_string(s, c, arr, envp);
-	if (token_num < 0)
-		return (free_cmd(arr), NULL);
+        if (!s)
+                return (NULL);
+        arr = (char **)malloc((token_count(s, c) + 1) * sizeof(char *));
+        if (!arr)
+                return (NULL);
+        token_num = fill_arr_from_string(s, c, arr, envp);
+        if (token_num < 0)
+                return (free_cmd(arr), NULL);
         arr[token_num] = NULL;
         arr = split_expanded_tokens(arr);
         if (!arr)
@@ -132,11 +131,17 @@ char	**tokenize_command(char const *s, char c, char **envp)
         arr = split_redirs(arr);
         if (!arr)
                 return (NULL);
-	i = 0;
-	while (arr[i])
-	{
-		remove_quotes(arr[i]);
-		i++;
-	}
-	return (arr);
+        token_num = count_strings(arr);
+        *quoted = malloc(sizeof(int) * token_num);
+        if (!*quoted)
+                return (free_cmd(arr), NULL);
+        i = 0;
+        while (i < token_num)
+        {
+                (*quoted)[i] = (ft_strchr(arr[i], '\'') || ft_strchr(arr[i], '"'));
+                remove_quotes(arr[i]);
+                i++;
+        }
+        return (arr);
 }
+
--- src/piping/redirections.c
+++ src/piping/redirections.c
@@ -14,123 +14,126 @@
 #include "minishell.h"
 #include <errno.h>
 
-int	open_infile(char *file, int *in_fd)
+int     open_infile(char *file, int *in_fd)
 {
-	int	fd;
+        int     fd;
 
-	fd = 0;
-	if (*in_fd != STDIN_FILENO)
-		close(*in_fd);
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-	{
-		perror(file);
-		if (errno == EACCES)
-			g_exit_code = 126;
-		else if (errno == ENOENT)
-			g_exit_code = 127;
-		else
-			g_exit_code = 1;
-		return (-1);
-	}
-	*in_fd = fd;
-	return (0);
+        fd = 0;
+        if (*in_fd != STDIN_FILENO)
+                close(*in_fd);
+        fd = open(file, O_RDONLY);
+        if (fd < 0)
+        {
+                perror(file);
+                if (errno == EACCES)
+                        g_exit_code = 126;
+                else if (errno == ENOENT)
+                        g_exit_code = 127;
+                else
+                        g_exit_code = 1;
+                return (-1);
+        }
+        *in_fd = fd;
+        return (0);
 }
 
-int	open_outfile(char *file, int *out_fd)
+int     open_outfile(char *file, int *out_fd)
 {
-	int	fd;
+        int     fd;
 
-	fd = 0;
-	if (*out_fd != STDOUT_FILENO)
-		close(*out_fd);
-	fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd < 0)
-	{
-		perror(file);
-		if (errno == EACCES)
-			g_exit_code = 126;
-		else
-			g_exit_code = 1;
-		return (-1);
-	}
-	*out_fd = fd;
-	return (0);
+        fd = 0;
+        if (*out_fd != STDOUT_FILENO)
+                close(*out_fd);
+        fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd < 0)
+        {
+                perror(file);
+                if (errno == EACCES)
+                        g_exit_code = 126;
+                else
+                        g_exit_code = 1;
+                return (-1);
+        }
+        *out_fd = fd;
+        return (0);
 }
 
-int	open_appendfile(char *file, int *out_fd)
+int     open_appendfile(char *file, int *out_fd)
 {
-	int	fd;
+        int     fd;
 
-	fd = 0;
-	if (*out_fd != STDOUT_FILENO)
-		close(*out_fd);
-	fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	if (fd < 0)
-	{
-		perror(file);
-		if (errno == EACCES)
-			g_exit_code = 126;
-		else
-			g_exit_code = 1;
-		return (-1);
-	}
-	*out_fd = fd;
-	return (0);
+        fd = 0;
+        if (*out_fd != STDOUT_FILENO)
+                close(*out_fd);
+        fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        if (fd < 0)
+        {
+                perror(file);
+                if (errno == EACCES)
+                        g_exit_code = 126;
+                else
+                        g_exit_code = 1;
+                return (-1);
+        }
+        *out_fd = fd;
+        return (0);
 }
 
-int	handle_redirection_logic(char **cmd, int *in_fd, int *out_fd, int *i)
+static int      handle_redirection_logic(char **cmd, int *quoted, char **envp,
+                int *in_fd, int *out_fd, int *i)
 {
-	if (!ft_strcmp(cmd[*i], "<") && cmd[*i + 1])
-	{
-		if (open_infile(cmd[*i + 1], in_fd) == -1)
-			return (-1);
-	}
-	else if (!ft_strcmp(cmd[*i], "<<") && cmd[*i + 1])
-	{
-		if (handle_heredoc(cmd[*i + 1], in_fd) == -1)
-			return (-1);
-	}
-	else if (!ft_strcmp(cmd[*i], ">") && cmd[*i + 1])
-	{
-		if (open_outfile(cmd[*i + 1], out_fd) == -1)
-			return (-1);
-	}
-	else if (!ft_strcmp(cmd[*i], ">>") && cmd[*i + 1])
-	{
-		if (open_appendfile(cmd[*i + 1], out_fd) == -1)
-			return (-1);
-	}
-	else
-		return (0);
-	(*i) += 2;
-	return (1);
+        if (!ft_strcmp(cmd[*i], "<") && cmd[*i + 1])
+        {
+                if (open_infile(cmd[*i + 1], in_fd) == -1)
+                        return (-1);
+        }
+        else if (!ft_strcmp(cmd[*i], "<<") && cmd[*i + 1])
+        {
+                if (handle_heredoc(cmd[*i + 1], quoted[*i + 1], envp, in_fd) == -1)
+                        return (-1);
+        }
+        else if (!ft_strcmp(cmd[*i], ">") && cmd[*i + 1])
+        {
+                if (open_outfile(cmd[*i + 1], out_fd) == -1)
+                        return (-1);
+        }
+        else if (!ft_strcmp(cmd[*i], ">>") && cmd[*i + 1])
+        {
+                if (open_appendfile(cmd[*i + 1], out_fd) == -1)
+                        return (-1);
+        }
+        else
+                return (0);
+        (*i) += 2;
+        return (1);
 }
 
-char	**handle_redirections(char **cmd, int count, int *in_fd, int *out_fd)
+char    **handle_redirections(char **cmd, int *quoted, int count, char **envp,
+                int *in_fd, int *out_fd)
 {
-	int		i;
-	int		j;
-	char	**clean;
-	int		res;
+        int             i;
+        int             j;
+        char    **clean;
+        int             res;
 
-	i = 0;
-	j = 0;
-	clean = malloc(sizeof(char *) * (count + 1));
-	if (!clean)
-		return (NULL);
-	while (cmd[i])
-	{
-		res = handle_redirection_logic(cmd, in_fd, out_fd, &i);
-		if (res == -1)
-		{
-			free(clean);
-			return (NULL);
-		}
-		else if (res == 0)
-			clean[j++] = cmd[i++];
-	}
-	clean[j] = NULL;
-	free(cmd);
-	return (clean);
+        i = 0;
+        j = 0;
+        clean = malloc(sizeof(char *) * (count + 1));
+        if (!clean)
+                return (NULL);
+        while (cmd[i])
+        {
+                res = handle_redirection_logic(cmd, quoted, envp, in_fd, out_fd, &i);
+                if (res == -1)
+                {
+                        free(clean);
+                        return (NULL);
+                }
+                else if (res == 0)
+                        clean[j++] = cmd[i++];
+        }
+        clean[j] = NULL;
+        free(cmd);
+        return (clean);
 }
+
